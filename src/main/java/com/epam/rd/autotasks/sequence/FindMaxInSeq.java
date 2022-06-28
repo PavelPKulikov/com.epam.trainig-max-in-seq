3105e2e17813bbf8ed652a4c3c049d2af68d6bab
Pavel Kulikov <ppkulikov@gmail.com>
2022-06-28 18:56:52 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
+++ src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
@@ -3,10 +3,27 @@ import java.util.Scanner;
 
 public class FindMaxInSeq {
     public static int max() {
-
-        // Put your code here
-
-        return 0;
+        int[] seq = new int[100];
+        int i = 0;
+        Scanner scan = new Scanner(System.in);
+        String sequence = scan.nextLine();
+        for (String s: sequence.split(" ")) {
+            seq[i] = Integer.parseInt(s);
+            i++;
+            if (Integer.parseInt(s) == 0) {
+                break;
+            }
+        }
+        i--;
+        int maxSq = seq[0];
+        int j = 1;
+        while (j < i){
+            if (seq[j]>maxSq){
+                maxSq = seq[j];
+            }
+            j++;
+        }
+        return maxSq;
     }
 
     public static void main(String[] args) {
